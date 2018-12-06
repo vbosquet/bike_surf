1d58c6226f8f4579161c06a4f198d9461e2fb9da
Virginie Bosquet <vivi_sander@hotmail.com>
2018-12-06 18:53:39 +0100

    Adding previous step link

--- app/controllers/rent_a_bike_controller.rb
+++ app/controllers/rent_a_bike_controller.rb
@@ -1,20 +1,19 @@
 class RentABikeController < ApplicationController
   include Wicked::Wizard
-	steps :add_description, :add_pricings, :add_availabilities
+	steps :add_description, :add_pricing, :add_availability
 
   def show
     case step
     when :add_description
-      @listing = Listing.new
-      @listing.build_bike
-      @listing.build_location
-      session[:listing] = nil
-    when :add_pricings
+      session[:listing].nil? ? @listing = Listing.new : @listing = Listing.new(session[:listing])
+      session[:bike].nil? ? @listing.build_bike : @listing.build_bike(session[:bike])
+      session[:location].nil? ? @listing.build_location : @listing.build_location(session[:location])
+    when :add_pricing
       @listing = Listing.new(session[:listing])
-      @listing.build_pricing
-    when :add_availabilities
+      session[:pricing].nil? ? @listing.build_pricing : @listing.build_pricing(session[:pricing])
+    when :add_availability
       @listing = Listing.new(session[:listing])
-      @listing.build_availability
+      session[:availability].nil? ? @listing.build_availability : @listing.build_availability(session[:availability])
     end
     render_wizard
   end
@@ -25,23 +24,40 @@ class RentABikeController < ApplicationController
       @listing = Listing.new(listing_params)
       @bike = @listing.build_bike(listing_params[:bike_attributes])
       @location = @listing.build_location(listing_params[:location_attributes])
-      session[:listing] = @listing.attributes
-      session[:bike] = @bike.attributes
-      session[:location] = @location.attributes
-      redirect_to next_wizard_path
-    when :add_pricings
+
+      if params[:commit] == "Annuler"
+        session[:listing] = nil
+        session[:bike] = nil
+        session[:location] = nil
+  			redirect_to listings_path
+      else
+        session[:listing] = @listing.attributes
+        session[:bike] = @bike.attributes
+        session[:location] = @location.attributes
+        redirect_to next_wizard_path
+  		end
+    when :add_pricing
       @listing = Listing.new(session[:listing])
       @pricing = @listing.build_pricing(listing_params[:pricing_attributes])
-      session[:pricing] = @pricing.attributes
-      redirect_to next_wizard_path
-    when :add_availabilities
-      @listing = Listing.new(session[:listing])
-      @listing.save
-      @listing.create_bike(session[:bike])
-      @listing.create_location(session[:location])
-      @listing.create_pricing(session[:pricing])
-      @listing.create_availability(listing_params[:availability_attributes])
-      redirect_to listing_path(@listing)
+
+      if params[:commit] == "Annuler"
+  			redirect_to previous_wizard_path
+      else
+        session[:pricing] = @pricing.attributes
+        redirect_to next_wizard_path
+  		end
+    when :add_availability
+      if params[:commit] == "Annuler"
+  			redirect_to previous_wizard_path
+      else
+        @listing = Listing.new(session[:listing])
+        @listing.save
+        @listing.create_bike(session[:bike])
+        @listing.create_location(session[:location])
+        @listing.create_pricing(session[:pricing])
+        @listing.create_availability(listing_params[:availability_attributes])
+        redirect_to listing_path(@listing)
+  		end
     end
   end
 
